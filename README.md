c6ca30f1aff1c2775b2df7d6429638db0b1e66b2
Victor Mak <contact@htetaung.com>
2024-06-17 19:18:02 +0800

    feat: basic interaction

---
+++ README.md
@@ -0,0 +1,19 @@
+# Langchain Java Server
+
+> An experiment on executing prompts with Claude API using Java Spring Web
+
+Albeit it is very fast to set up Langchain and expose its functionalities to the web via libraries like FastAPI or Django, Python is comparatively slow compared to compiled languages like Java.
+
+The target of this experiment is to ensure the feasibility of using Langchain in Java.
+
+It seems that Langchain4J does not have all the features I was hoping to implement for Claude like Function Calling based on OpenAPI Specification.
+
+Even then, I would say this is a good experiment as I was able to experiment
+1. Langchain4J
+2. Java Spring
+
+for the first time in my life.
+## Tools and Frameworks Used
+- IDEA Commuity
+- Java Spring
+- [Langchain4J](https://github.com/langchain4j/langchain4j)
--- src/main/java/com/htetaung/langchain_java_server/langchain_java_server/LangchainJavaServerApplication.java
+++ src/main/java/com/htetaung/langchain_java_server/langchain_java_server/LangchainJavaServerApplication.java
@@ -1,23 +1,41 @@
 package com.htetaung.langchain_java_server.langchain_java_server;
 
+import dev.langchain4j.model.anthropic.AnthropicChatModel;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class LangchainJavaServerApplication {
 
+	@Value("${CLAUDE_API}")
+	private String claudeApiKey;
+
 	public static void main(String[] args) {
 		SpringApplication.run(LangchainJavaServerApplication.class, args);
 	}
 
+	@PostConstruct
+	public void init() {
+		DataContainer.model = AnthropicChatModel.withApiKey(claudeApiKey);
+	}
+}
+
+class DataContainer{
+	public static AnthropicChatModel model;
 }
 
 @RestController
 class HelloController{
+
 	@GetMapping("/")
-	static String index(){
-		return "Hello World";
+	public String index(){
+		String answer = DataContainer.model.generate("My name is Ethan. Say Hello {my name}'");
+		return answer;
 	}
 }
